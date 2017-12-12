747f7500f350c2222a56c2eab9b17daf20b4565c
Jaeheon Jee <32578710+jaeheonjee@users.noreply.github.com>
2017-12-12 16:09:46 +0900

    Gazuaaaaa

--- process.py
+++ process.py
@@ -1,9 +1,13 @@
 import os
+import csv
 import time
 import requests
 
-DATA_30M = 'https://bitcoincharts.com/charts/chart.json?m=bitstampUSD&SubmitButton=Draw&r=10&i=30-min&c=0&' + \
-           's=&e=&Prev=&Next=&t=S&b=&a1=&m1=10&a2=&m2=25&x=0&i1=&i2=&i3=&i4=&v=1&cv=0&ps=0&l=0&p=0&'
+from datetime import datetime
+
+
+DATA_30M = 'https://bitcoincharts.com/charts/chart.json?m=bitstampUSD&SubmitButton=Draw&r=10&i=30-min&c=1&s=2017-11-01' + \
+        '&e=2017-12-13&Prev=&Next=&t=S&b=&a1=&m1=10&a2=&m2=25&x=0&i1=&i2=&i3=&i4=&v=1&cv=0&ps=0&l=0&p=0&'
 TICKER_URL = 'https://www.bitstamp.net/api/ticker/'
 
 def main():
@@ -14,12 +18,16 @@ def main():
         while True:
             req = requests.get(TICKER_URL)
             body = req.json()
-            writeCSV(body['last'])
+            writeCSV(body)
 
             # sleep(1800)
-            time.sleep(1800)
+            time.sleep(10)
     except:
         pass
+
+def convertTime(timestamp):
+    d = datetime.fromtimestamp(timestamp)
+    return d.strftime('%Y-%m-%d %H:%M')
 
 def create():
     if not os.path.exists('./bitcoin.txt'):
@@ -27,16 +35,21 @@ def create():
         body = req.json()
 
         with open('./bitcoin.txt', 'w') as f:
-            f.write('LAST\n')
+            fieldnames = ['DATE', 'LAST']
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
 
-            for line in body:
-                f.write("{}\n".format(line[1]))
+            writer.writeheader()
+            for line in body[:-1]:
+                writer.writerow({'DATE': convertTime(line[0]), 'LAST': line[1]})
 
 
 def writeCSV(data):
     try:
         with open('./test.txt', 'a') as f:
-            f.write("{}\n".format(data))
+            fieldnames = ['DATE', 'LAST']
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writerow({'DATE': convertTime(data['timestamp']), 'LAST': data['last']})
+
     except:
         print("Error: Failed to write csv file.")
 
